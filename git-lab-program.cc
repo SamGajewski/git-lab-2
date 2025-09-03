9e1227f48ba898abc04eacbe287fa655fb61ffb4
SamGajewski <Samuel.a.gajewski@gmail.com>
2025-09-03 17:50:00 -0400

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <SamGajewski>
+ *        Date: <09/03/2025>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    return 0;
+}// main
+cout << "Hello Git!!" << endl;
